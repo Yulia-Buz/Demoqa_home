fe6212f32ec77578fb2968968a88a2220d6aa8e2
Yulia_B <buzynnikova.yulia@yandex.ru>
2023-09-18 13:51:49 +0300

    2nd commit

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -4,6 +4,6 @@ from pages.swag_labs import Labs
 def test_icon_exist(browser):
     demoqa_home_page = Labs(browser)
     demoqa_home_page.visit()
-    demoqa_home_page.username()
-    demoqa_home_page.password()
+    assert demoqa_home_page.username()
+    assert demoqa_home_page.password()
     assert demoqa_home_page.exist_icon()
